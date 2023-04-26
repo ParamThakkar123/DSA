692604855470769d5f5d960f5b8a4892b0984865
ParamThakkar123 <128291516+ParamThakkar123@users.noreply.github.com>
2023-04-26 23:56:47 +0530

    Create GCDusingBitManipulation.cpp
    In this program we find the value of GCD using Euclid's Algorithm and Bit manipulation Concepts. This method is much faster than the iterative and recursive approach.

---
+++ GCDusingBitManipulation.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int gcd(int a, int b) {
+    if (!a || !b)
+        return a | b;
+    unsigned shift = __builtin_ctz(a | b);
+    a >>= __builtin_ctz(a);
+    do {
+        b >>= __builtin_ctz(b);
+        if (a > b)
+            swap(a, b);
+        b -= a;
+    } while (b);
+    return a << shift;
+}
+
+int main(){
+    int a, b;
+    cin >> a >> b;
+    cout << gcd(a,b) << endl;
+}
