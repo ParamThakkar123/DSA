aee545c41f4c0bc100832844f9d19b94ade33ee8
Param Thakkar <128291516+ParamThakkar123@users.noreply.github.com>
2023-04-27 18:40:07 +0530

    Update ExtendedEuclideanGCDalgo.cpp

--- ExtendedEuclideanGCDalgo.cpp
+++ ExtendedEuclideanGCDalgo.cpp
@@ -13,7 +13,7 @@ int gcd(int a, int b, int *x, int *y)
     }
 
     int x1, y1;
-    int gcd = gcdExtended(b%a, a, &x1, &y1);
+    int gcd = gcd(b%a, a, &x1, &y1);
 
     *x = y1 - (b/a) * x1;
     *y = x1;
