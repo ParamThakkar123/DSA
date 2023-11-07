b2f84bbbdd77c059ca7fc90936f53ca19a2fbe98
Param Thakkar <128291516+ParamThakkar123@users.noreply.github.com>
2023-11-07 12:45:09 +0530

    Create Minimum Value in BST.cpp

---
+++ Graphs/Trees/Minimum Value in BST.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    struct Node* parent;
+
+    Node(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+        parent = NULL;
+    }
+};
+
+int treemin(Node* root) {
+    if(root == NULL){
+        return -1;
+    }
+    
+    else if(root->left == NULL){
+        return root->data;
+    }
+    
+    else if(root->left != NULL){
+        return treemin(root->left);
+    }
+    
+    return root->data;
+}
+
+
+int main(){
+    struct Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+    
+    cout << treemin(root);
+}
