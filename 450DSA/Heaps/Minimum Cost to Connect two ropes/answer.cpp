dd0bcb1c45acbc8cd8b06bec724b25f6af9145d9
Param Thakkar <128291516+ParamThakkar123@users.noreply.github.com>
2023-12-24 16:03:00 +0530

    Create answer.cpp

---
+++ 450DSA/Heaps/Minimum Cost to Connect two ropes/answer.cpp
@@ -0,0 +1,24 @@
+long long minCost(long long arr[], long long n) {
+        priority_queue<long long, vector<long long>, greater<long long>> pq;
+        
+        for(int i = 0; i < n; i++){
+            pq.push(arr[i]);
+        }
+        
+        long long cost = 0;
+        
+        while(pq.size() > 1){
+            long long a = pq.top();
+            pq.pop();
+            
+            long long b = pq.top();
+            pq.pop();
+            
+            long long sum = a + b;
+            cost += sum;
+            
+            pq.push(sum);
+        }
+        
+        return cost;
+    }
