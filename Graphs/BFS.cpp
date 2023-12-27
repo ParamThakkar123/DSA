5c2dd440d3b939882306adfbe9ddb609a1dacb5f
Param Thakkar <128291516+ParamThakkar123@users.noreply.github.com>
2023-12-27 18:17:40 +0530

    Update BFS.cpp

--- Graphs/BFS.cpp
+++ Graphs/BFS.cpp
@@ -1,60 +1,65 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Graph{
-  int numofvertices;
-  list<int>* adjList;
-  bool* visited;
-  
-  public:
-    Graph(int vertices);
-    void AddEdge(int src, int dest);
-    void BFS(int startVertex);
+class Graph {
+ 
+    int V;
+    vector<list<int> > adj;
+ 
+public:
+    Graph(int V);
+    void addEdge(int v, int w);
+    void BFS(int s);
+};
+ 
+Graph::Graph(int V)
+{
+    this->V = V;
+    adj.resize(V);
 }
-
-Graph::Graph(int vertices){
-  numVertice = vertices;
-  adjList = new list<int>[vertices];
+ 
+void Graph::addEdge(int v, int w)
+{
+    adj[v].push_back(w);
 }
+ 
+void Graph::BFS(int s)
+{
+    vector<bool> visited;
+    visited.resize(V, false);
+    list<int> queue;
+  
+    visited[s] = true;
+    queue.push_back(s);
+ 
+    while (!queue.empty()) {
 
-void Graph:: AddEdge(int src, int dest){
-  visited = new bool[numVertices];
-  for(int i = 0; i < numVertices; i++){
-    visited[i] = false;
-  }
-  
-  list<int> queue;
-  
-  visited[startVertex] = true;
-  queue.push_back(startVertex);
-  
-  list<int>::iterator i;
-  
-  while(!queue.empty()){
-    int currVertex = q.front();
-    cout << "Visited" << currVertex << " ";
-    queue.pop_front();
-  }
-  
-  for(int i = adjList[currVertex].begin(); i != adjList[currVertex].end(); i++){
-    int adjVertex = *i;
-    if(!visited[adjVertex]){
-      visited[adjVertex] = true;
-      queue.push_back(adjVertex);
+        s = queue.front();
+        cout << s << " ";
+        queue.pop_front();
+ 
+        for (auto adjacent : adj[s]) {
+            if (!visited[adjacent]) {
+                visited[adjacent] = true;
+                queue.push_back(adjacent);
+            }
+        }
     }
-  }
 }
 
-int main(){
-  Graph g(4);
-  g.addEdge(0, 1);
-  g.addEdge(0, 2);
-  g.addEdge(1, 2);
-  g.addEdge(2, 0);
-  g.addEdge(2, 3);
-  g.addEdge(3, 3);
-
-  g.BFS(2);
-
-  return 0;
+int main()
+{
+    Graph g(4);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 2);
+    g.addEdge(2, 0);
+    g.addEdge(2, 3);
+    g.addEdge(3, 3);
+ 
+    cout << "Following is Breadth First Traversal "
+         << "(starting from vertex 2) \n";
+    g.BFS(2);
+ 
+    return 0;
 }
