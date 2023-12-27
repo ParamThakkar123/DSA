e6ece81fbb339e532d75435769ddc2077c46c97d
Param Thakkar <128291516+ParamThakkar123@users.noreply.github.com>
2023-12-27 12:29:56 +0530

    Create Search in Rotated sorted array.cpp

---
+++ Searching/Search in Rotated sorted array.cpp
@@ -0,0 +1,31 @@
+int search(int* arr, int n, int target) {
+    int low = 0;
+    int high = n - 1;
+
+    while(low <= high){
+        int mid = low + (high - low) / 2;
+        if(arr[mid] == target){
+            return mid;
+        }
+
+        if(arr[low] <= arr[mid]){
+            if(target >= arr[low] && target <= arr[mid]){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+
+        else{
+            if(target >= arr[mid] && target <= arr[high]){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+    }
+
+    return -1;
+}
